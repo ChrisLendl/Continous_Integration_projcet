a42a6f8e3439d3ba532e3e4271cbbd5a9d3c3ebe
Christian Lendl <c.lendl@web.de>
2021-12-19 14:09:54 +0100

    update whole repository

---
+++ src/unittest/python/build_main_file_tests.py
@@ -0,0 +1,24 @@
+"""test twitter program"""
+
+import unittest
+from mock import Mock
+import buildmainfile #import main program
+
+
+class TweetTest(unittest.TestCase):
+
+    def test_example(self):
+        """test funktion of main program without posting onto twitter (via Mock()"""
+        mock_twitter = Mock() #instance of mock
+        buildmainfile.tweet(mock_twitter, "message") #mock_twitter instead of api from twitter
+        mock_twitter.PostUpdate.assert_called_with("message") #call assert_called_with method and put in expected message
+
+    def test_example2(self):
+        """another test"""
+        mock_twitter = Mock()
+        buildmainfile.tweet(mock_twitter, "Hello this is a message")
+        mock_twitter.PostUpdate.assert_called_with("message")
+
+
+if __name__ == '__main__':
+    unittest.main()
